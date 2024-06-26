dc743dd0aee371ab46b03444c5d6dab8c59d7ac2
anurag-kochar <anuragmarketing101@gmail.com>
2024-06-26 12:10:00 +0530

    ⚒  add: base header, and layout component && symbol groups data

---
+++ src/constants/symbol-groups.ts
@@ -0,0 +1,39 @@
+import type { SymbolGroups } from "../types";
+
+export const SYMBOL_GROUPS: SymbolGroups = {
+  frontend: [
+    {
+      name: "React Js",
+      logo: "https://utfs.io/f/1fdff40e-7f5d-4749-8a8b-35ecb2b63d89-1sj3pb.svg",
+      landingPageUrl: "",
+    },
+  ],
+  backend: [
+    {
+      name: "Node Js",
+      logo: "https://utfs.io/f/0aafb458-87c6-48c5-94fa-11f9ba89f91a-20lc2.svg",
+      landingPageUrl: "",
+    },
+  ],
+  database: [
+    {
+      name: "React Js",
+      logo: "https://utfs.io/f/1fdff40e-7f5d-4749-8a8b-35ecb2b63d89-1sj3pb.svg",
+      landingPageUrl: "",
+    },
+  ],
+  deployment: [
+    {
+      name: "Vercel",
+      logo: "https://utfs.io/f/09f60d39-9525-4759-b35a-b959695265d1-dk6j3t.svg",
+      landingPageUrl: "",
+    },
+  ],
+  styling: [
+    {
+      name: "Material UI",
+      logo: "https://utfs.io/f/5297be7f-abf0-4070-917e-dfdae2c312c3-2bpd.svg",
+      landingPageUrl: "",
+    },
+  ],
+};
---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export type Symbol_ = {
+  name: string;
+  logo: string;
+  landingPageUrl: string;
+};
+
+export type SymbolGroups = {
+  frontend: Symbol_[];
+  backend: Symbol_[];
+  database: Symbol_[];
+  styling: Symbol_[];
+  deployment: Symbol_[];
+};
