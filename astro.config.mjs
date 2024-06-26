3c7f42215b47d1e33668dbec75d877aee590234b
anurag-kochar <anuragmarketing101@gmail.com>
2024-06-26 11:44:16 +0530

    ⚒  add: tailwind css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from "astro/config";
 import partytown from "@astrojs/partytown";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ],
+  integrations: [partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), tailwind()]
 });
